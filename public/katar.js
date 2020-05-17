d3454facc825618946ad6c76c5c2105cca0e6acb
Mehmet Unal <MehmetUnal@Mehmets-Mac-Pro-2.local>
2020-05-17 03:09:14 +0300

    Initial commit

---
+++ public/katar.js
@@ -0,0 +1,73 @@
+
+
+let r ;
+let g ;
+let b ;
+let y ;
+
+
+
+function setup(){
+
+createCanvas(innerWidth, innerHeight);
+background(239,240,241)
+frameRate(60);
+
+r = random (0 , 255);
+g = random (0 , 255);
+b = random (0 , 255);
+
+
+socket = io.connect('http://localhost:3000')
+socket.on('mouse' , newDrawing);
+
+// y = socket.ids;
+
+// console.log(y);
+
+// if (y == 0 ){
+//     y = random (0 , 255)
+// }
+}
+
+
+
+
+function newDrawing(data){
+
+    fill(200, 250,18)
+    stroke(58, 61, 75)
+    ellipse(data.x, data.y, 60, 60)
+}
+
+
+function mouseDragged(){
+    console.log('sending: ' + mouseX + ',' + mouseY);
+
+
+    var data = {
+        x : mouseX,
+        y : mouseY
+    }
+
+    socket.emit ('mouse' , data);
+
+    fill(159, 156, 155)
+    stroke(58, 61, 75)
+    ellipse(mouseX, mouseY, 60, 60)
+
+
+}
+
+
+
+
+function draw(){
+}
+
+
+
+
+
+
+
