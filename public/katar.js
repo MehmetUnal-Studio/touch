2e23fb5e45d9d0252a769b6192a3ace9c9d0302e
MEHMET UNAL <62450956+MehmetUnal-Studio@users.noreply.github.com>
2020-09-23 19:23:01 +0300

    Update katar.js

--- public/katar.js
+++ public/katar.js
@@ -1,132 +1,66 @@
-/*
-* @name Drawing
-* @description Generative painting program.
-*/
 
-// All the paths
-let paths = [];
-// Are we painting?
-let painting = false;
-// How long until the next circle
-let next = 0;
-// Where are we now and where were we?
-let current;
-let previous;
 
-function setup() {
-  createCanvas(720, 400);
-  current = createVector(0,0);
-  previous = createVector(0,0);
-};
+let r ;
+let g ;
+let b ;
+let y ;
 
-function draw() {
-  background(200);
-  
-  // If it's time for a new point
-  if (millis() > next && painting) {
 
-    // Grab mouse position      
-    current.x = mouseX;
-    current.y = mouseY;
 
-    // New particle's force is based on mouse movement
-    let force = p5.Vector.sub(current, previous);
-    force.mult(0.05);
+function setup(){
 
-    // Add new particle
-    paths[paths.length - 1].add(current, force);
-    
-    // Schedule next circle
-    next = millis() + random(100);
+createCanvas(innerWidth, innerHeight);
+background(239,240,241)
+frameRate(60);
 
-    // Store mouse values
-    previous.x = current.x;
-    previous.y = current.y;
-  }
+r = random (0 , 255);
+g = random (0 , 255);
+b = random (0 , 255);
 
-  // Draw all paths
-  for( let i = 0; i < paths.length; i++) {
-    paths[i].update();
-    paths[i].display();
-  }
+
+socket = io.connect('http://localhost:3000')
+socket.on('mouse' , newDrawing);
+
+// y = socket.ids;
+
+// console.log(y);
+
+// if (y == 0 ){
+//     y = random (0 , 255)
+// }
 }
 
-// Start it up
-function mousePressed() {
-  next = 0;
-  painting = true;
-  previous.x = mouseX;
-  previous.y = mouseY;
-  paths.push(new Path());
+
+
+
+function newDrawing(data){
+
+    fill(200, 250,18)
+    stroke(58, 61, 75)
+    ellipse(data.x, data.y, 60, 60)
 }
 
-// Stop
-function mouseReleased() {
-  painting = false;
-}
 
-// A Path is a list of particles
-class Path {
-  constructor() {
-    this.particles = [];
-    this.hue = random(100);
-  }
+function mouseDragged(){
+    console.log('sending: ' + mouseX + ',' + mouseY);
 
-  add(position, force) {
-    // Add a new particle with a position, force, and hue
-    this.particles.push(new Particle(position, force, this.hue));
-  }
-  
-  // Display plath
-  update() {  
-    for (let i = 0; i < this.particles.length; i++) {
-      this.particles[i].update();
+
+    var data = {
+        x : mouseX,
+        y : mouseY
     }
-  }  
-  
-  // Display plath
-  display() {    
-    // Loop through backwards
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-      // If we shold remove it
-      if (this.particles[i].lifespan <= 0) {
-        this.particles.splice(i, 1);
-      // Otherwise, display it
-      } else {
-        this.particles[i].display(this.particles[i+1]);
-      }
-    }
-  
-  }  
+
+    socket.emit ('mouse' , data);
+
+    fill(159, 156, 155)
+    stroke(58, 61, 75)
+    ellipse(mouseX, mouseY, 60, 60)
+
+
 }
 
-// Particles along the path
-class Particle {
-  constructor(position, force, hue) {
-    this.position = createVector(position.x, position.y);
-    this.velocity = createVector(force.x, force.y);
-    this.drag = 0.95;
-    this.lifespan = 255;
-  }
 
-  update() {
-    // Move it
-    this.position.add(this.velocity);
-    // Slow it down
-    this.velocity.mult(this.drag);
-    // Fade it out
-    this.lifespan--;
-  }
 
-  // Draw particle and connect it with a line
-  // Draw a line to another
-  display(other) {
-    stroke(0, this.lifespan);
-    fill(0, this.lifespan/2);    
-    ellipse(this.position.x,this.position.y, 8, 8);    
-    // If we need to draw a line
-    if (other) {
-      line(this.position.x, this.position.y, other.position.x, other.position.y);
-    }
-  }
+
+function draw(){
 }
